ebe96c8d15c9e1e8006c3b7c21c3201782c966e6
lasahony35 <steven.deleonfeliz@gmail.com>
2024-05-16 22:10:12 -0400

    v0.04 mejora TkidByName

--- tests/GRC/EPIC/Flujo_Rescate_Cuota/BABDP-2446/tests.spec.ts
+++ tests/GRC/EPIC/Flujo_Rescate_Cuota/BABDP-2446/tests.spec.ts
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 import { BasePage } from '../../../../../POM/base/base.page';
 import { GRCPage } from '../../../../../POM/GRC/GRC.page';
-import {TkIdByName} from '../../../../../utils/processInspector/TkIdByName'
+import { TkIdByName } from '../../../../../utils/processInspector/TkIdByName'
 import { AsignarTask } from '../../../../../utils/asignarTask';
 test.describe('BABDP-2446 | Yo como usuario del proceso necesito tener un historial de ejecución de la solicitud', () => {
 
@@ -15,7 +15,7 @@ test.describe('BABDP-2446 | Yo como usuario del proceso necesito tener un histor
         let TaskId;
 
         await test.step('Abrir Instancia GRC', async () => {
-        
+
             await BASE.NewInstance('Gestión Rescate de Cuotas')
         })
 
@@ -23,28 +23,25 @@ test.describe('BABDP-2446 | Yo como usuario del proceso necesito tener un histor
             instanceID = await GRC.flujoCrearSolicitud()
         })
         await test.step('Avanzar Crear Solicitud', async () => {
-            await GRC.GRCElements.getIdInstancia().waitFor({state:'visible'})
+            await GRC.GRCElements.getIdInstancia().waitFor({ state: 'visible' })
             await GRC.GRCElements.getAvanzar().click()
- 
-            await GRC.GRCElements.getIdInstancia().waitFor({state:'hidden'})
+
+            await GRC.GRCElements.getIdInstancia().waitFor({ state: 'hidden' })
 
 
-            if(instanceID !== undefined && instanceID !== 0){
-                await page.waitForTimeout(5000)
-                 TaskId = await TkIdByName(instanceID,'Validar Disponibilidad')
-            } 
+            TaskId = await TkIdByName(instanceID, 'Validar Disponibilidad')
 
             console.log(await TaskId)
-            await AsignarTask(instanceID,'Validar Disponibilidad')
-           
-         })
-         await test.step('Abrir tarea "Validar Disponibilidad"', async () => {
+            await AsignarTask(instanceID, 'Validar Disponibilidad')
+
+        })
+        await test.step('Abrir tarea "Validar Disponibilidad"', async () => {
             await page.goto(`https://bpd.automationcloud.ibm.com/dba/test/ProcessPortal/launchTaskCompletion?taskId=${TaskId}`)
             await BASE.InTask('Gestión Rescate de Cuotas')
-           await page.pause();
-         })
+            await page.pause();
+        })
 
-      
+
     });
 
 
--- utils/processInspector/TkIdByName.ts
+++ utils/processInspector/TkIdByName.ts
@@ -1,40 +1,45 @@
 import { TypesTasksByInstance } from '../../interfaces/TasksByInstance.types';
-
 import cookiesJSON from '../../config/auth/auth.json';
+
 const cookiesArray = cookiesJSON.cookies;
 
-export const TkIdByName = async (instanceID: number, name: string): Promise<number | string> => {
-
+export const TkIdByName = async (instanceID: number, name: string): Promise<number> => {
     const url = `https://bpd.automationcloud.ibm.com/dba/test/rest/bpm/wle/v1/process/${instanceID}/taskSummary/All?offset=0&limit=50`;
-
     const cookiesString = cookiesArray.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-
     const requestOptions = {
         method: 'GET',
         headers: {
             'Cookie': cookiesString
         }
     };
-    try {
-        const response: TypesTasksByInstance = await fetch(url, requestOptions)
-            .then(response => response.json())
-            .catch(error => console.error('Error en fetch:', error));
 
+    const maxAttempts = 7;
+    for (let attempt = 1; attempt <= maxAttempts; attempt++) {
+        try {
+            const response: TypesTasksByInstance = await fetch(url, requestOptions)
+                .then(response => response.json())
+                .catch(error => {
+                    console.error('Error en fetch:', error);
+                    throw error;
+                });
 
-        const tareasFiltradas = response.data.tasks.filter(task => task.name.trim().replace(/\s{2,}/g, ' ') === name);
+            const tareasFiltradas = response.data.tasks.filter(task => task.name.trim().replace(/\s{2,}/g, ' ') === name);
 
+            if (tareasFiltradas.length > 0) {
+                return parseInt(tareasFiltradas[0].tkiid);
+            }
 
-        if (tareasFiltradas.length > 0) {
-            return tareasFiltradas[0].tkiid
-        } else {
-            return 0
+            console.log(`Intento ${attempt} fallido: tarea no encontrada.`);
+        } catch (error) {
+            console.error(`Error en el intento ${attempt}:`, error);
+            if (attempt === maxAttempts) {
+                throw new Error('Número máximo de intentos alcanzado y la tarea no se encontró.');
+            }
         }
-    } catch (error) {
-        throw error
+
+        // Esperar un breve periodo antes de intentar de nuevo
+        await new Promise(resolve => setTimeout(resolve, 1000)); // 1 segundo de espera
     }
 
-}
-
-
-
-
+    return 0; // O lanzar un error si prefieres
+};
