dcbc5c079de8f0110b890707faa5a3a843e5947c
elpibedev <steven.deleonfeliz@gmail.com>
2024-11-09 19:18:31 -0400

    tes

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["allure-playwright"]],
+  reporter: [["null"], ["allure-playwright"]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
--- tests/prueba.spec.ts
+++ tests/prueba.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { GRCPage  } from '../POM/GRC/GRC.page';
 import { BasePage } from '../POM/base/base.page';
+import { AsignarTask } from '../utils/asignarTask';
 
 
 
-test('prueba', async ({ page }) => {
+test('login', async ({ page }) => {
     const GRC = new GRCPage(page)
     const base = new BasePage(page)
 
-    await base.NewInstance('Gestión Rescate de Cuotas')
-    await page.screenshot()
+   await AsignarTask(12441,'Debida Diligencia')
+   
     // await GRC.flujoCrearSolicitud()
     // await page.pause()
 })
